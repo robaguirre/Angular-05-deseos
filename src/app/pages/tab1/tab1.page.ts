2f03266cfe3db0d664ed7f50ea49d8750f394dda
Roberto Agirre <r.agirre@ibermatica.com>
2019-11-14 17:33:30 +0100

    Pantalla y nav

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DeseosService } from '../../services/deseos.service';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +10,43 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor( public deseosService: DeseosService,
+               private router: Router,
+               private alertCrtl: AlertController) {
+
+  }
+
+  async agregarLista() {
+
+    const alert = await this.alertCrtl.create({
+      header: 'Nueva lista',
+      inputs: [{
+        name: 'titulo',
+        type: 'text',
+        placeholder: 'Nombre de la lista'
+      }],
+      buttons: [{
+        text: 'Cancelar',
+        role: 'cancel',
+        handler: () => console.log('Cancelar')
+      },
+      {
+        text: 'Crear',
+        handler: (data) => {
+            if (data.titulo.length === 0){
+              return;
+            }
+
+            // Crear la lista
+            this.deseosService.crearLista(data.titulo);
+        }
+      }]
+    });
+
+    alert.present();
+
+    //this.router.navigateByUrl('/tabs/tab1/agregar');
+
+  }
 
 }
